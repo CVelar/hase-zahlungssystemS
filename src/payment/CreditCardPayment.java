462194489604026696f2cb0f4c064eb4605185e4
chuck <c.jacob7410@gmail.com>
2025-11-24 14:02:56 +0100

    müll entfernt

--- src/payment/CreditCardPayment.java
+++ src/payment/CreditCardPayment.java
@@ -4,14 +4,10 @@ import bankingEntity.*;
 
 public class CreditCardPayment implements PaymentMethod {
     private String cardNumber;
-    private String cardHolderName;
-    private String expiryDate;
     private String cvv;
 
     public CreditCardPayment(String cardNumber, String cardHolderName, String expiryDate, String cvv) {
         this.cardNumber = cardNumber;
-        this.cardHolderName = cardHolderName;
-        this.expiryDate = expiryDate;
         this.cvv = cvv;
     }
 
